6d3f0ea604a2211077c82e6ebb34af42cc1c59af
neostreet <lloyd.aidan@gmail.com>
2024-10-11 12:45:33 -0400

    add data files

---
+++ poker_styles_data.sql
@@ -0,0 +1,104 @@
+use poker
+
+delete from poker_styles;
+
+insert into poker_styles (
+  poker_style,
+  poker_style_descr,
+  poker_style_abbrev
+)
+values (
+  0,
+  'Cash game',
+  'Cash '
+);
+
+insert into poker_styles (
+  poker_style,
+  poker_style_descr,
+  poker_style_abbrev
+)
+values (
+  1,
+  'Sit & Go',
+  'SNG  '
+);
+
+insert into poker_styles (
+  poker_style,
+  poker_style_descr,
+  poker_style_abbrev
+)
+values (
+  2,
+  'MTT',
+  'MTT  '
+);
+
+insert into poker_styles (
+  poker_style,
+  poker_style_descr,
+  poker_style_abbrev
+)
+values (
+  3,
+  'Spin & Go',
+  'Sp&Go'
+);
+
+insert into poker_styles (
+  poker_style,
+  poker_style_descr,
+  poker_style_abbrev
+)
+values (
+  4,
+  'KO poker',
+  'KO   '
+);
+
+insert into poker_styles (
+  poker_style,
+  poker_style_descr,
+  poker_style_abbrev
+)
+values (
+  5,
+  'Power up',
+  'POW  '
+);
+
+insert into poker_styles (
+  poker_style,
+  poker_style_descr,
+  poker_style_abbrev
+)
+values (
+  6,
+  'MTT with rebuys',
+  'MTT-R'
+);
+
+insert into poker_styles (
+  poker_style,
+  poker_style_descr,
+  poker_style_abbrev
+)
+values (
+  7,
+  'MTT with bounties',
+  'MTT-B'
+);
+
+insert into poker_styles (
+  poker_style,
+  poker_style_descr,
+  poker_style_abbrev
+)
+values (
+  8,
+  'MTT with guaranteed prize pool',
+  'MTT-G'
+);
+
+quit
