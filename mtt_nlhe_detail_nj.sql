7ebd91ea7bc8daadbeacaee1619ad957d92c5a84
neostreet <lloyd.aidan@gmail.com>
2024-10-27 15:01:18 -0400

    add buy_in + entry_fee

--- mtt_nlhe_detail_nj.sql
+++ mtt_nlhe_detail_nj.sql
@@ -1,6 +1,6 @@
 use poker_nj
 
-select delta,buy_in,entry_fee,num_entries,places_paid,place,winnings,concat(poker_session_date,tournament_letter)
+select delta,(round(buy_in + entry_fee,0)) bi_ef,buy_in,entry_fee,num_entries,places_paid,place,winnings,concat(poker_session_date,tournament_letter)
 from poker_sessions
 where poker_style = 2 and poker_flavor = 3
 order by id;
