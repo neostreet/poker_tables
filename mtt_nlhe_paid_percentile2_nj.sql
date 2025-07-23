a86e5996bfdc56eaac4c8cda4bf6ae25bb02cc00
neostreet <lloyd.aidan@gmail.com>
2025-07-23 12:59:03 -0400

    catch up with local changes

--- mtt_nlhe_paid_percentile2_nj.sql
+++ mtt_nlhe_paid_percentile2_nj.sql
@@ -1,6 +1,7 @@
 use poker_nj
 
-select (num_entries - places_paid) / num_entries,num_entries,places_paid,place,winnings,
+select ((num_entries - places_paid) / num_entries) percentile,
+num_entries,places_paid,place,winnings,
 concat(poker_session_date,tournament_letter)
 from poker_sessions
 where poker_style = 2 and poker_flavor = 3 and
