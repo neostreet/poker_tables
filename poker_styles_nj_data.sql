38a4800c6f9a8f078c947f2cab2b1a0407d22a2e
neostreet <lloyd.aidan@gmail.com>
2025-08-02 19:29:41 -0400

    add num_hands

--- poker_styles_nj_data.sql
+++ poker_styles_nj_data.sql
@@ -1,104 +1,70 @@
 use poker_nj
-
-delete from poker_styles;
-
-insert into poker_styles (
-  poker_style,
-  poker_style_descr,
-  poker_style_abbrev
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,num_rebuys,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
 )
-values (
-  0,
-  'Cash game',
-  'Cash '
-);
-
-insert into poker_styles (
-  poker_style,
-  poker_style_descr,
-  poker_style_abbrev
+values (2,'a','2023-09-13',27.30,2.70,1,5000,9,3,0,0,84,13,130);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
 )
-values (
-  1,
-  'Sit & Go',
-  'SNG  '
-);
-
-insert into poker_styles (
-  poker_style,
-  poker_style_descr,
-  poker_style_abbrev
+values (2,'b','2023-09-13',18.20,1.80,5000,9,3,15,0,82,13,46);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
 )
-values (
-  2,
-  'MTT',
-  'MTT  '
-);
-
-insert into poker_styles (
-  poker_style,
-  poker_style_descr,
-  poker_style_abbrev
+values (2,'c','2023-09-13',13.65,1.35,5000,9,3,42,0,152,23,62);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
 )
-values (
-  3,
-  'Spin & Go',
-  'Sp&Go'
-);
-
-insert into poker_styles (
-  poker_style,
-  poker_style_descr,
-  poker_style_abbrev
+values (2,'a','2023-09-14',9.10,0.90,5000,8,3,32,0,156,20,68);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
 )
-values (
-  4,
-  'KO poker',
-  'KO   '
-);
-
-insert into poker_styles (
-  poker_style,
-  poker_style_descr,
-  poker_style_abbrev
+values (2,'b','2023-09-14',4.55,0.45,5000,9,3,28,0,163,26,98);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
 )
-values (
-  5,
-  'Power up',
-  'POW  '
-);
-
-insert into poker_styles (
-  poker_style,
-  poker_style_descr,
-  poker_style_abbrev
+values (2,'a','2024-01-20',4.55,0.45,2500,6,3,42,0,91,14,8);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
 )
-values (
-  6,
-  'MTT with rebuys',
-  'MTT-R'
-);
-
-insert into poker_styles (
-  poker_style,
-  poker_style_descr,
-  poker_style_abbrev
+values (2,'b','2024-01-20',13.65,1.35,5000,9,3,0,0,208,35,31);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
 )
-values (
-  7,
-  'MTT with bounties',
-  'MTT-B'
-);
-
-insert into poker_styles (
-  poker_style,
-  poker_style_descr,
-  poker_style_abbrev
+values (2,'c','2024-01-20',4.55,0.45,2500,6,3,33,0,84,14,23);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
 )
-values (
-  8,
-  'MTT with guaranteed prize pool',
-  'MTT-G'
-);
-
+values (2,'a','2024-01-21',27.30,2.70,5000,9,3,27,0,104,17,30);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
+)
+values (2,'a','2024-01-22',27.30,2.70,5000,9,3,22,0,118,20,77);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
+)
+values (2,'a','2024-01-23',27.30,2.70,5000,9,3,33,0,121,20,40);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
+)
+values (2,'a','2024-01-24',9.10,0.90,5000,9,3,42,0,226,39,69);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
+)
+values (2,'b','2024-01-24',27.30,2.70,5000,9,3,0,0,90,15,46);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
+)
+values (2,'c','2024-01-24',18.20,1.80,5000,9,3,24,0,89,15,39);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
+)
+values (2,'a','2024-10-26',27.30,2.70,5000,9,3,0,0,79,13,9);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
+)
+values (2,'a','2024-10-27',9.10,0.90,5000,9,3,32,0,189,31,56);
+insert into poker_sessions (
+  poker_style,tournament_letter,poker_session_date,buy_in,entry_fee,initial_stake,num_players,poker_flavor,place,winnings,num_entries,places_paid,num_hands
+)
+values (2,'b','2024-10-27',18.20,1.80,5000,9,3,14,0,79,13,44);
 quit
