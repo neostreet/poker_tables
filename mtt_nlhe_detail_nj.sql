4d919d804c182da352eef5f7b30afabce4c36528
neostreet <lloyd.aidan@gmail.com>
2025-08-02 19:08:02 -0400

    add num_hands to the output

--- mtt_nlhe_detail_nj.sql
+++ mtt_nlhe_detail_nj.sql
@@ -1,6 +1,7 @@
 use poker_nj
 
-select delta,(round(buy_in + entry_fee,0)) bi_ef,buy_in,entry_fee,num_entries,places_paid,place,winnings,concat(poker_session_date,tournament_letter)
+select delta,(round(buy_in + entry_fee,0)) bi_ef,buy_in,entry_fee,num_entries,places_paid,place,winnings,num_hands,
+concat(poker_session_date,tournament_letter)
 from poker_sessions
 where poker_style = 2 and poker_flavor = 3
 order by id;
